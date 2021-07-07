199b8ff83a02fd1a8ffce878520a4f2dd3a61666
Luccas Mateus de Medeiros Gomes <luccasmmg@gmail.com>
2021-07-07 14:54:06 -0300

    stuff for deployment(url and fix on nginx.conf)

--- frontend/src/config/index.tsx
+++ frontend/src/config/index.tsx
@@ -1,2 +1,2 @@
-const BASE_URL: string = process.env.NODE_ENV === 'production' ? 'http://localhost:8000' : '';
+const BASE_URL: string = process.env.NODE_ENV === 'production' ? 'https://new.posgraduacao.ccsa.ufrn.br' : '';
 export default BASE_URL;
