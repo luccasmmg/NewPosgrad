c6e9d7c6075c424d68788c6a4220e6420e3a6cc6
Luccas Mateus de Medeiros Gomes <luccasmmg@gmail.com>
2021-08-11 11:39:04 -0300

    Delete useless file and changed base url

--- frontend/src/config/index.tsx
+++ frontend/src/config/index.tsx
@@ -1,2 +1,2 @@
-const BASE_URL: string = process.env.NODE_ENV === 'production' ? 'https://new.posgraduacao.ccsa.ufrn.br' : '';
+const BASE_URL: string = process.env.NODE_ENV === 'production' ? 'https://sdp.ccsa.ufrn.br' : '';
 export default BASE_URL;
